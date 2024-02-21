4e35ecd9660d1e4bb8b0663304ff2b94afdc76a4
Alexander Lebedev <hardskulls@rambler.ru>
2024-02-21 19:52:47 +0300

    refactor: Removed most common type conversions traits into `mappings` repo.

--- src/lib.rs
+++ src/lib.rs
@@ -22,8 +22,8 @@ Basic usage:
 ```
 use error_traits::MergeOkErr;
 
-let res : Result<String, String> = Ok("foo".to_owned());
-let merged_res : String = res.merge_ok_err();
+let res: Result<String, String> = Ok("foo".to_owned());
+let merged_res: String = res.merge_ok_err();
 ```
 */
 pub trait MergeOkErr<T> {
@@ -51,7 +51,7 @@ use error_traits::MapErrBy;
 struct MyError;
 
 let error = || MyError;
-let number : Result<u32, MyError> = "42".parse::<u32>().map_err_by(error);
+let number: Result<u32, MyError> = "42".parse::<u32>().map_err_by(error);
 ```
 */
 pub trait MapErrBy<T, N> {
@@ -75,7 +75,7 @@ Basic usage:
 ```
 use error_traits::{MapErrBy, MapErrToString};
 
-let number : Result<u32, String> = "42".parse::<u32>().map_err_to_str();
+let number: Result<u32, String> = "42".parse::<u32>().map_err_to_str();
 ```
 */
 pub trait MapErrToString<T> {
@@ -88,81 +88,6 @@ where
 {
     fn map_err_to_str(self) -> StdResult<T, String> {
         self.map_err(|e| e.to_string())
-    }
-}
-
-/**
-Wraps any type in `Ok` or `Err` variant of standard `Result` type.
-Helps to avoid parenthesis apocalypse.
-
-
-# Examples
-
-```
-use error_traits::WrapInRes;
-
-enum MyEnum
-{
-    Foo(u8),
-    Bar(u8)
-}
-
-// Instead of writing
-let e : Result<String, Option<MyEnum>> = Err(Some(MyEnum::Foo(0)));
-
-// you can write:
-let r : Result<String, Option<MyEnum>> = MyEnum::Foo(0).into().in_err();
-// or
-let r : Result<Option<MyEnum>, _> =
-    MyEnum::Foo(0)
-        .into()
-        .in_ok::<String>();
-```
-*/
-pub trait WrapInRes<T> {
-    fn in_ok<ERR>(self) -> Result<T, ERR>;
-    fn in_err<OK>(self) -> Result<OK, T>;
-}
-
-impl<T> WrapInRes<T> for T {
-    fn in_ok<ERR>(self) -> Result<T, ERR> {
-        Ok(self)
-    }
-    fn in_err<OK>(self) -> Result<OK, T> {
-        Err(self)
-    }
-}
-
-pub trait ToEmpty {
-    fn to_empty(self);
-}
-
-impl<T> ToEmpty for T {
-    fn to_empty(self) {}
-}
-
-/**
-Transforms one type into another.
-
-
-# Examples
-
-```
-use std::time::Duration;
-use error_traits::MapType;
-
-let minutes = 5;
-
-let duration : Duration = minutes.map_type(|m| Duration::from_secs(m * 60));
-```
-*/
-pub trait MapType<M, N> {
-    fn map_type(self, f: impl FnOnce(M) -> N) -> N;
-}
-
-impl<M, N> MapType<M, N> for M {
-    fn map_type(self, f: impl FnOnce(M) -> N) -> N {
-        f(self)
     }
 }
 
@@ -194,27 +119,3 @@ impl<T, E> PassErrWith for Result<T, E> {
         self
     }
 }
-
-/// Wraps type in `Option` and returns `None` if condition is true.
-pub trait ToNoneIf: Sized {
-    fn to_none_if(self, cond: impl Fn(&Self) -> bool) -> Option<Self> {
-        match cond(&self) {
-            true => None,
-            _ => Some(self),
-        }
-    }
-}
-
-impl<T> ToNoneIf for T {}
-
-/// Wraps type in `Result` and returns `Err` if condition is true.
-pub trait ToErrIf: Sized {
-    fn to_err_if<T>(self, cond: impl Fn(&Self) -> bool, err: T) -> Result<Self, T> {
-        match cond(&self) {
-            true => Err(err),
-            _ => Ok(self),
-        }
-    }
-}
-
-impl<T> ToErrIf for T {}
